f062071ece2095fe2d7f94a0c26b3cba69f3a58a
Sean Felling <Snfelling7@gmail.com>
2017-04-11 11:08:52 -0500

    final update

--- group_01.js
+++ group_01.js
@@ -6,3 +6,71 @@ var robert = ["Robert", "26835", "66000", 1];
 var mayella = ["Mayella", "89068", "35000", 2];
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
+
+// first attempt
+
+var ratingBonus = function (array){
+  var bonus = 0;
+  if (array[3]===3){
+    bonus += (0.04);
+    // return(bonus);
+  }
+  else if (array[3]===4){
+    bonus += (0.06);
+    // return(bonus);
+  }
+  else if (array[3]===5){
+    bonus += (0.10);
+    // return(bonus);
+  }
+  else {
+  bonus += 0;
+  // return(bonus);
+  }
+  // check senority
+  bonus += seniority(array[1]);
+  if (bonus > 0.13) {
+    bonus = 0.13;
+  }
+  // check for salary over 65,000
+  if (parseFloat (array[2])>65000 && array[3] > 2) {
+    bonus -= 0.01;
+  }
+    return bonus;
+};//end of ratingBonus
+
+var seniority = function (idNumber) {
+  if (idNumber.length===4) {
+    return 0.05;
+  } else {
+    return 0;
+  }
+};//end of seniority
+
+//checking for the percentage of overall bonus
+function finalSalary(array) {
+  return array[2] * (ratingBonus(array)+1);
+} //end finalSalary
+
+//checking for bonus amount
+
+
+//Building final array from employees array
+function finalArray(array) {
+  var employeesNewArray = [];
+  for (var i=0; i<array.length; i++) {
+    var arrayAti = array[i];
+    var innerArray = [];
+    // push in employees name
+    innerArray.push(arrayAti[0]);
+    innerArray.push(ratingBonus(arrayAti)*10);
+    innerArray.push(finalSalary(arrayAti));
+    innerArray.push(parseInt(ratingBonus(arrayAti)*arrayAti[2]));
+    employeesNewArray.push(innerArray);
+  }
+  return employeesNewArray;
+}
+
+
+
+console.log(finalArray(employees));
